c8db831b881aeb4b7ae8931fe5e6dd58cb973150
Abidhc <abidhasan2418@gmail.com>
2023-02-25 00:22:48 +0600

    Initial stage

--- lib/screen/onboarding/pinVerificationScreen.dart
+++ lib/screen/onboarding/pinVerificationScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../style/style.dart';
+
 class pinVerificationScreen extends StatefulWidget {
   const pinVerificationScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,33 @@ class pinVerificationScreen extends StatefulWidget {
 class _pinVerificationScreenState extends State<pinVerificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Pin Verification",style: Head1Text(colorDarkBlue),),
+                SizedBox(height: 1,),
+                Text("A 6 digit verification pin will send to your email address",style: Head6Text(colorLightGray),),
+                SizedBox(height: 20,),
+                Container(
+                  child: ElevatedButton(
+                    style:AppButtonStyle(),
+                    child:  SuccessButtonChild('Login'),
+                    onPressed: () {},
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screen/onboarding/setPasswordScreen.dart
+++ lib/screen/onboarding/setPasswordScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../style/style.dart';
+
 class setPasswordScreen extends StatefulWidget {
   const setPasswordScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,41 @@ class setPasswordScreen extends StatefulWidget {
 class _setPasswordScreenState extends State<setPasswordScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Set Password",style: Head1Text(colorDarkBlue),),
+                const SizedBox(height: 1,),
+                Text("Minimum length password 8 character with\n Letter or number combination ",style: Head6Text(colorLightGray),),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration('Set password'),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration('Confirm Password'),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  child: ElevatedButton(
+                    style:AppButtonStyle(),
+                    child:  SuccessButtonChild('confirm'),
+                    onPressed: () {},
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );;
   }
 }
